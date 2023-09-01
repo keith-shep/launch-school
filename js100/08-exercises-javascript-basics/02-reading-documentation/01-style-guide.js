2d8b5fca757cc6dd1f4e01ee5fad630fddc3aca2
Keith Shepherdson <kmson7@gmail.com>
2023-09-01 22:20:51 +0800

    Format code to JS guidelines

---
+++ js100/08-exercises-javascript-basics/02-reading-documentation/01-style-guide.js
@@ -0,0 +1,22 @@
+// Before
+let ice_cream_taste = 'chocolate'
+let ice_cream_density = 10
+
+while(ice_cream_density > 0)
+{
+    console.log('Drip...');
+    ice_cream_density -= 1;
+}
+
+console.log('The '+ ice_cream_taste +' ice cream melted.');
+
+// After
+let iceCreamTaste = 'chocolate';
+let iceCreamDensity = 10;
+
+while(iceCreamDensity > 0) {
+  console.log('Drip...');
+  iceCreamDensity -= 1;
+}
+
+console.log('The ' + iceCreamTaste + ' ice cream melted.');
