7749746af506b0bc40488afd41848f72cba2731b
Keith Shepherdson <kmson7@gmail.com>
2023-09-02 12:13:06 +0800

    Update 10-type-error.js

---
+++ js100/08-exercises-javascript-basics/02-reading-documentation/10-type-error.js
@@ -0,0 +1,13 @@
+// current code
+// let tweet = 'Woohoo! :-)';
+
+// if (tweet.length() > 140) {
+//   console.log('Tweet is too long!');
+// }
+
+// Fixed code
+let tweet = 'Woohoo! :-)';
+
+if (tweet.length > 140) {
+  console.log('Tweet is too long!');
+}
