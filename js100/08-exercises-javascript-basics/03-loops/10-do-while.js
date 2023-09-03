d41af93c3c4932efa1b8393b4ea04900e1e67def
Keith Shepherdson <kmson7@gmail.com>
2023-09-03 19:22:52 +0800

    Update 10-do-while.js

---
+++ js100/08-exercises-javascript-basics/03-loops/10-do-while.js
@@ -0,0 +1,18 @@
+// #while
+// let counter = 0;
+
+// while (counter > 0) {
+//   console.log('Woooot!');
+//   counter -= 1;
+// }
+
+// #do while
+// let counter = 0;
+
+// do {
+//   console.log('Woooot!');
+//   counter -= 1;
+// } while (counter > 0);
+
+
+// do while will run at least once.
