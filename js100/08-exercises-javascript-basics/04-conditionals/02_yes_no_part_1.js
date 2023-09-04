b796b0745ef9c6a279da5cfa166c09a9b2ac3bf7
Keith Shepherdson <kmson7@gmail.com>
2023-09-04 22:44:48 +0800

    Update 02_yes_no_part_1.js

--- js100/08-exercises-javascript-basics/04-conditionals/02_yes_no_part_1.js
+++ js100/08-exercises-javascript-basics/04-conditionals/02_yes_no_part_1.js
@@ -1,7 +1,15 @@
 let randomNumber = Math.round(Math.random());
 
-if (randomNumber === 1) {
+// if (randomNumber === 1) {
+//   console.log('Yes!');
+// } else if (randomNumber === 0) {
+//   console.log('No.');
+// }
+
+// alternative
+
+if (randomNumber) {
   console.log('Yes!');
-} else if (randomNumber === 0) {
+} else {
   console.log('No.');
 }
