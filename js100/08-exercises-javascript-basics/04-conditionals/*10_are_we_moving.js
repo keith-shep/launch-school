64c302f951550cd34cf2aa12a0a943180ba244e9
Keith Shepherdson <kmson7@gmail.com>
2023-09-05 23:13:11 +0800

    Add star to filename

--- js100/08-exercises-javascript-basics/04-conditionals/*10_are_we_moving.js
+++ js100/08-exercises-javascript-basics/04-conditionals/*10_are_we_moving.js
@@ -3,8 +3,9 @@ let acceleration = 24;
 let brakingForce = 19;
 
 let isMoving = brakingForce < acceleration && (speed > 0 || acceleration > 0);
-    isMoving = true && (false || true);
-    isMoving = true;
+    // isMoving = true && (false || true);
+    // isMoving = true;
 
+  console.log(isMoving);
 
 // output true
