a51c2b276017dbb9cb715f929f0bd8a3412912e5
Keith Shepherdson <kmson7@gmail.com>
2023-09-05 23:22:08 +0800

    Update 03-cite-the-author.js

---
+++ js100/08-exercises-javascript-basics/05-functions-1/03-cite-the-author.js
@@ -0,0 +1,10 @@
+// cite('Brendan Eich', 'Always bet on JavaScript.');
+// logs:
+// Brendan Eich said: "Always bet on JavaScript."
+
+
+function cite(author, quote) {
+  console.log(`${author} said: "${quote}"`);
+}
+
+cite('Brendan Eich', 'Always bet on JavaScript.');
