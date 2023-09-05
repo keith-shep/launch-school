db76a0f8249433009f31d32a2a4f348527727955
Keith Shepherdson <kmson7@gmail.com>
2023-09-06 00:08:41 +0800

    Delete 09-locale-part-1.js

--- js100/08-exercises-javascript-basics/05-functions-1/09-locale-part-1.js
+++
@@ -1,7 +0,0 @@
-function extractLanguage(locale) {
-  return locale.substring(0, 2);
-}
-
-console.log(extractLanguage('en_US.UTF-8'));  // 'en'
-console.log(extractLanguage('en_GB.UTF-8'));  // 'en'
-console.log(extractLanguage('ko_KR.UTF-16')); // 'ko'
